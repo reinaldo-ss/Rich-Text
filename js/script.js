341a2256a600764b7ac4acd9fd0c11da2e6d2671
Reinaldo da Silva <118370294+reinaldo-ss@users.noreply.github.com>
2024-02-23 23:19:39 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ let formatButtons = document.querySelectorAll(".format");
 
 let scriptButtons = document.querySelectorAll(".script");
 
-/* Lista das Fontes */
+// Lista das Fontes
 
 let fontList = [
     "Arial",
@@ -30,8 +30,9 @@ let fontList = [
     "cursive",
 ];
 
-/* Configurações iniciais */
+// Configurações iniciais
 const initializer = () => {
+
 /* Chamando as funções dos botões de destaque e sem destaque 
 para link, unlink, lists, desfazer, refazer, pois são operações únicas */
 highlighter(alignButtons, true);
@@ -39,3 +40,41 @@ highlighter(spacingButtons, true);
 highlighter(formatButtons, false);
 highlighter(scriptButtons,true);
 };
+
+// Destacando botão clicado
+const highlighter = (className, needsRemoval) => {
+    className.forEach((button) => {
+        button.addEventListener("click", () => {
+            /* True significa que apenas um botão deve ser 
+            destacado e outro seria normal */
+            if(needsRemoval) {
+                let alreadyActive = false;
+
+            // Se o botão atualmente clicado já estiver ativo
+            if(button.classList.contains("active")) {
+                alreadyActive = true;
+            }
+
+            //Removendo os destaques(highlighter) de outros botões
+            highlighterRemover(className);
+            if(!alreadyActive){
+                //Destacar botão clicado
+                button.classList.add("active"); 
+            }
+            }
+            else{
+                //se outros botões podem ser destacados
+                button.classList.toggle("active");
+            }
+        });
+    });
+};
+
+// Função para remover o destaque
+const highlighterRemover = (className) => {
+    className.forEach((button) => {
+        button.classList.remove("active")
+    });
+}
+
+window.onload = initializer();
